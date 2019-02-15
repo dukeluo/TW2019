f1d094200b59ac58ab83ead119456714120a5d36
duke <dukeluo@outlook.com>
2019-02-15 16:27:09 +0800

    complete the example given 0 km and 0 min

--- week03/spec/main-spec.js
+++ week03/spec/main-spec.js
@@ -1,5 +1,11 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it("returns the number 0 when given 0 km distance and 0 min parking wait", function () {
+        let distance = 0;
+        let parkingTime = 0;
+        let result = main(distance, parkingTime);
+
+        expect(result).toEqual(0);
+    });
 });
