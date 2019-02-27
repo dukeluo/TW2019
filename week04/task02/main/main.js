3d2b6d3e339d5820e216337e233f274a13780744
duke <dukeluo@outlook.com>
2019-02-27 16:22:30 +0800

    modify printInventory to match tasking

--- week04/task02/main/main.js
+++ week04/task02/main/main.js
@@ -3,79 +3,92 @@ const loadAllItems = database.loadAllItems;
 const loadPromotions = database.loadPromotions;
 
 
-/*
- * using an array to record the information that needs to be printed of a item
- *
- * information is recorded in the following order:
- * [name, unit, price, count, cost, freeCount, diff]
- */
-function printInventory(inputs) {
-    let inputsDataObj = {};
-    let items = loadAllItems();
-    let promotions = loadPromotions();
+function fixedTwoDec(num) {
+    return num.toFixed(2);
+}
 
-    inputs.forEach(function (item) {
+
+function tidyInput(inputs) {
+    return inputs.sort().reduce((countMap, item) => {
         if (item.indexOf("-") !== -1) {
-            let a = item.split("-");
+            let [name, count] = item.split("-");
 
-            if (inputsDataObj.hasOwnProperty(a[0])) {
-                inputsDataObj[a[0]] += (+a[1]);
-            } else {
-                inputsDataObj[a[0]] = (+a[1]);
-            }
+            countMap[name] = +count;
         } else {
-            if (inputsDataObj.hasOwnProperty(item)) {
-                inputsDataObj[item]++;
-            } else {
-                inputsDataObj[item] = 1;
-            }
+            countMap[item] = ++countMap[item] || 1;
         }
-    });
-    for (let barcode in inputsDataObj) {
-        let arr = [];
-        let item = items.find(function (i) {
-            return i.barcode === barcode;
-        });
-        let count = (+inputsDataObj[barcode]);
-        let cost = count * item.price;
+        return countMap;
+    }, {});
+}
+
+
+function turnIntoPrintedObjArray(countMap, allItems, allPromotions) {
+    let objArray = [];
+    let buyTwoOneFreeBarcodes = allPromotions[0].barcodes;
+
+    for( [barcode, count] of Object.entries(countMap)) {
+        let {name, unit, price} = allItems.find(e => e.barcode === barcode);
+        let cost = price * count;
         let freeCount = 0;
         let diff = 0;
 
-        if (promotions[0].barcodes.indexOf(barcode) !== -1) {
+        if (buyTwoOneFreeBarcodes.indexOf(barcode) !== -1) {
             freeCount = Number.parseInt(count/3);
-            diff = freeCount * item.price;
+            diff = freeCount * price;
             cost -= diff;
         }
-        arr = arr.concat(item.name, item.unit, item.price, count, cost, freeCount, diff);
-        inputsDataObj[barcode] = arr;
+        objArray.push({barcode, name, unit, price, count, cost, freeCount, diff});
+    }
+    return objArray;
+}
+
+
+function buildBillText(printedObjArray) {
+    let itemStr = printedObjArray.map(e => `名称：${e.name}，数量：${e.count}${e.unit}，单价：${fixedTwoDec(e.price)}(元)，小计：${fixedTwoDec(e.cost)}(元)`);
+    let freeStr = printedObjArray.filter(e => e.freeCount > 0)
+                                 .map(e => `名称：${e.name}，数量：${e.freeCount}${e.unit}`);
+    let {sum, less} = printedObjArray.reduce((auxObj, curr) => {
+        auxObj.sum += curr.cost;
+        auxObj.less += curr.diff;
+        return auxObj;
+    }, {sum: 0, less: 0});
+
+    if (freeStr.length) {
+        freeStr = ["挥泪赠送商品：", ...freeStr, "----------------------"];
+    }
+    return ["***<没钱赚商店>购物清单***",
+            ...itemStr,
+            "----------------------",
+            ...freeStr,
+            `总计：${fixedTwoDec(sum)}(元)`,
+            `节省：${fixedTwoDec(less)}(元)`,
+            `**********************`].join("\n");
+}
+
+
+function printInventory(inputs) {
+    let items = loadAllItems();
+    let promotions = loadPromotions();
+
+    if (!items) {
+        console.error("Items is null or undefined!");
+        return ;
+    }
+    if (!promotions) {
+        console.error("Promotions is null or undefined!");
+        return ;
+    }
+    if (!inputs || !inputs.length) {
+        console.error("Input useless!");
+        return ;
     }
 
+    let array = turnIntoPrintedObjArray(tidyInput(inputs), items, promotions);
+    let text = buildBillText(array);
 
-    let printedText = "***<没钱赚商店>购物清单***\n";
-    let freeText = "";
-    let sum = 0;
-    let less = 0;
-
-    for (let barcode in inputsDataObj) {
-        let item = inputsDataObj[barcode];
-
-        printedText += `名称：${item[0]}，数量：${item[3]}${item[1]}，单价：${item[2].toFixed(2)}(元)，小计：${item[4].toFixed(2)}(元)\n`;
-        sum += item[4];
-        if (item[6]) {
-            freeText += `名称：${item[0]}，数量：${item[5]}${item[1]}\n`;
-            less += item[6];
-        }
-    }
-    printedText += "----------------------\n";
-    printedText += "挥泪赠送商品：\n";
-    printedText += freeText;
-    printedText += "----------------------\n";
-    printedText += `总计：${sum.toFixed(2)}(元)\n`;
-    printedText += `节省：${less.toFixed(2)}(元)\n`;
-    printedText += '**********************';
-    console.log(printedText);
+    console.log(text);
     return 1;
-};
+}
 
 
 module.exports = printInventory;
