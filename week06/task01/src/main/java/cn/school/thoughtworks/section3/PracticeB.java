445ac67f2115ee278efbdf985deeb1b6aff0fc51
duke <dukeluo@outlook.com>
2019-03-11 20:12:37 +0800

    feat: done praticeB of section3

--- week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -2,11 +2,17 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> map = new HashMap(collectionA);
 
-        return null;
+        for (String s : object.get("value")) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s)-map.get(s)/3);
+            }
+        }
+        return map;
     }
 }
