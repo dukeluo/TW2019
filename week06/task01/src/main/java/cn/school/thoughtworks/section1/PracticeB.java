b36f87624e110b0f756d70915bf82feba4286c11
duke <dukeluo@outlook.com>
2019-03-11 16:39:00 +0800

    feat: done practiceB of section1

--- week06/task01/src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> stringList = new ArrayList<String>();
+        List<String> intersection = new ArrayList<String>(collection1);
+
+        for (List<String> l : collection2) {
+            stringList.addAll(l);
+        }
+        intersection.retainAll(stringList);
+        return intersection;
     }
 }
