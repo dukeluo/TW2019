932857e7b8926e82823d60c5a815854f760d3b8c
duke <dukeluo@outlook.com>
2019-03-11 19:52:11 +0800

    feat: done practiceC of section2

--- week06/task01/src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -2,11 +2,44 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> countMap = new HashMap();
 
-        return null;
+        for (String s : collection1) {
+            if (s.length() != 1) {
+                multiCharsHandler(s, countMap);
+            } else {
+                increaseCount(s, 1, countMap);
+            }
+        }
+        return countMap;
+    }
+
+    public static void multiCharsHandler(String s, Map<String, Integer> map) {
+        int index;
+        int auxIndex;
+
+        if (s.contains("-")) {
+            index = s.indexOf("-");
+            increaseCount(s.substring(0, index), Integer.parseInt(s.substring(index+1)), map);
+        } else if (s.contains(":")) {
+            index = s.indexOf(":");
+            increaseCount(s.substring(0, index), Integer.parseInt(s.substring(index+1)), map);
+        } else if (s.contains("[") && s.contains("]")) {
+            index = s.indexOf("[");
+            auxIndex = s.indexOf("]");
+            increaseCount(s.substring(0, index), Integer.parseInt(s.substring(index+1, auxIndex)), map);
+        }
+    }
+
+    public static void increaseCount(String item, int num, Map<String, Integer> map) {
+        if (map.containsKey(item)) {
+            map.put(item, map.get(item)+num);
+        } else {
+            map.put(item, num);
+        }
     }
 }
