c595ea88f0bba539954c86ef0908cf9c43c6defc
duke <dukeluo@outlook.com>
2019-03-17 20:33:13 +0800

    feat: done Filter

--- week06/task02/src/main/java/com/thoughtworks/collection/Filter.java
+++ week06/task02/src/main/java/com/thoughtworks/collection/Filter.java
@@ -4,28 +4,37 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.*;
 
 public class Filter {
 
     List<Integer>  array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                         .filter(n -> n%2 == 0)
+                         .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return this.array.stream()
+                         .filter(n -> n%3 == 0)
+                         .collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                   .distinct()
+                   .collect(Collectors.toList());
+    }
+
+    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
+        return firstList.stream()
+                        .filter(secondList::contains)
+                        .collect(Collectors.toList());
     }
 }
