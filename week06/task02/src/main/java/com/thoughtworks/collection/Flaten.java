6eb5b46db21bc1bef59fe05273660acb68bb711d
duke <dukeluo@outlook.com>
2019-03-17 22:06:47 +0800

    feat: done Flaten

--- week06/task02/src/main/java/com/thoughtworks/collection/Flaten.java
+++ week06/task02/src/main/java/com/thoughtworks/collection/Flaten.java
@@ -3,19 +3,26 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.*;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Stream.of(this.array)
+                     .flatMap(Stream::of)
+                     .collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return this.transformToOneDimesional()
+                   .stream()
+                   .distinct()
+                   .collect(Collectors.toList());
     }
 }
