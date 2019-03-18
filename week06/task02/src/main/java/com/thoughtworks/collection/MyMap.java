afb616032d95c104271c4121d75424eb8e6ba16e
duke <dukeluo@outlook.com>
2019-03-18 10:32:51 +0800

    feat: done MyMap

--- week06/task02/src/main/java/com/thoughtworks/collection/MyMap.java
+++ week06/task02/src/main/java/com/thoughtworks/collection/MyMap.java
@@ -4,6 +4,8 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.*;
+import java.util.Collections;
 
 public class MyMap {
 
@@ -12,27 +14,48 @@ public class MyMap {
             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     private List<String> letterList = Arrays.asList(letters);
 
+    private static String tenToTwentySixBaseOne(int n) {
+        String str = "";
+        int m;
+
+        while (n > 0) {
+            m = n%26 == 0 ? 26 : n%26;
+            str = (char) (m+96) + str;
+            n = (n-m) / 26;
+        }
+        return str;
+    }
+
     public MyMap(List<Integer> array) {
         this.array = array;
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                         .map(n -> n*3)
+                         .collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return this.mapLetters();
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                .map(MyMap::tenToTwentySixBaseOne)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> resultList = this.sortFromSmall();
+
+        Collections.reverse(resultList);
+        return resultList;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return this.array.stream()
+                         .sorted()
+                         .collect(Collectors.toList());
     }
 }
