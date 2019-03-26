78c688aed54e29ffdc9669dc345f196d5fff8b12
duke <dukeluo@outlook.com>
2019-03-26 14:21:59 +0800

    feat: done Practice02

--- week07/task01/src/main/java/practice02/Person.java
+++ week07/task01/src/main/java/practice02/Person.java
@@ -1,4 +1,7 @@
 package practice02;
 
-public class Person {
+public class Person extends practice01.Person {
+    public Person(String name, int age) {
+        super(name, age);
+    }
 }
--- week07/task01/src/main/java/practice02/Student.java
+++ week07/task01/src/main/java/practice02/Student.java
@@ -1,4 +1,18 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.", klass);
+    }
 }
