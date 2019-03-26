aa58c5cff94145a95f0d9c34e26f0c54c3576d46
duke <dukeluo@outlook.com>
2019-03-26 14:19:26 +0800

    fix(Person): make constructor public

--- week07/task01/src/main/java/practice01/Person.java
+++ week07/task01/src/main/java/practice01/Person.java
@@ -4,7 +4,7 @@ public class Person {
     int age;
     String name;
 
-    Person(String name, int age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
