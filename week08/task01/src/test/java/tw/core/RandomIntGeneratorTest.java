f3601f7f22da9c80aa78eb8d5201cc3380451299
duke <dukeluo@outlook.com>
2019-04-12 15:59:58 +0800

    test: done test of RandomIntGenerator class

--- week08/task01/src/test/java/tw/core/RandomIntGeneratorTest.java
+++ week08/task01/src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,44 @@
 package tw.core;
 
+import tw.core.generator.RandomIntGenerator;
 
-/**
- * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
- */
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
 public class RandomIntGeneratorTest {
+    private RandomIntGenerator randomIntGenerator;
 
+    @Before
+    public void setUp() throws Exception {
+        randomIntGenerator = new RandomIntGenerator();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void should_throw_an_IllegalArgumentException_when_digitmax_is_less_than_numbersOfNeed() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        randomIntGenerator.generateNums(3, 4);
+    }
+
+    @Test
+    public void should_return_a_string_of_four_digits_less_than_ten() throws Exception {
+        String numString = randomIntGenerator.generateNums(9, 4);
+        String [] digits = numString.split(" ");
+        boolean allLessThanTen = true;
+
+        for (String s : digits) {
+            if (Integer.parseInt(s) > 9) {
+                allLessThanTen = false;
+                break;
+            }
+        }
+        assertEquals(4, digits.length);
+        assertEquals(true, allLessThanTen);
+    }
 }
