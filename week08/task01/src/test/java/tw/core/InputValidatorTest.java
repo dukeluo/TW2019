1152592804c7c04b2006ca9de3a753120c114ebe
duke <dukeluo@outlook.com>
2019-04-12 14:36:20 +0800

    test: done test of InputValidator class

--- week08/task01/src/test/java/tw/core/InputValidatorTest.java
+++ week08/task01/src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,49 @@
 package tw.core;
 
-/**
- * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
- */
-public class InputValidatorTest {
+import tw.validator.InputValidator;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InputValidatorTest {
+    private InputValidator inputValidator;
+
+    @Before
+    public void setUp() throws Exception {
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void should_return_false_when_input_number_count_is_less_than_four() throws Exception {
+        String input = "1 2";
+        boolean result = inputValidator.validate(input);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void should_return_false_when_input_number_have_duplicate_member() throws Exception {
+        String input = "1 1 2 3";
+        boolean result = inputValidator.validate(input);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void should_return_false_when_input_number_have_member_greater_than_nine() throws Exception {
+        String input = "1 2 3 10";
+        boolean result = inputValidator.validate(input);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void should_return_true_when_input_number_meet_the_input_requirements() throws Exception {
+        String input = "1 5 6 7";
+        boolean result = inputValidator.validate(input);
+
+        assertEquals(true, result);
+    }
 }
