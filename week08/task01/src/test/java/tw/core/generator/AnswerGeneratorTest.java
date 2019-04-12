d2f61580bd8cbfc6d6ab708a90314218686d63c5
duke <dukeluo@outlook.com>
2019-04-12 18:05:29 +0800

    test: done test of AnswerGenerator class

--- week08/task01/src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ week08/task01/src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,48 @@
 package tw.core.generator;
 
-/**
- * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
- */
-public class AnswerGeneratorTest {
+import tw.core.RandomIntGeneratorTest;
+import tw.core.generator.RandomIntGenerator;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.Answer;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+
+public class AnswerGeneratorTest {
+    private RandomIntGenerator mockedRandomIntGenerator;
+    private AnswerGenerator answerGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        mockedRandomIntGenerator = mock(RandomIntGenerator.class);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void should_throw_a_OutOfRangeAnswerException() throws Exception {
+        when(mockedRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 10");
+
+        answerGenerator = new AnswerGenerator(mockedRandomIntGenerator);
+        thrown.expect(OutOfRangeAnswerException.class);
+        answerGenerator.generate();
+    }
+
+    @Test
+    public void should_return_an_answer_object() throws Exception {
+        when(mockedRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 4");
+
+        answerGenerator = new AnswerGenerator(mockedRandomIntGenerator);
+        Answer answer = answerGenerator.generate();
+        assertEquals("1 2 3 4", answer.toString());
+    }
 }
 
