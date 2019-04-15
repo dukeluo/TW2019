ae757a2f221f7c168d5d460a55e37dc42526f248
duke <dukeluo@outlook.com>
2019-04-15 16:09:23 +0800

    refactor: modify the identifier s to str

--- week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -8,9 +8,9 @@ public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         Map<String, Integer> map = new HashMap(collectionA);
 
-        for (String s : object.get("value")) {
-            if (map.containsKey(s)) {
-                map.put(s, map.get(s)-1);
+        for (String str : object.get("value")) {
+            if (map.containsKey(str)) {
+                map.put(str, map.get(str)-1);
             }
         }
         return map;
--- week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -8,9 +8,9 @@ public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         Map<String, Integer> map = new HashMap(collectionA);
 
-        for (String s : object.get("value")) {
-            if (map.containsKey(s)) {
-                map.put(s, map.get(s)-map.get(s)/3);
+        for (String str : object.get("value")) {
+            if (map.containsKey(str)) {
+                map.put(str, map.get(str)-map.get(str)/3);
             }
         }
         return map;
