ac97a625d69184c0f46218550da32926232cab0e
duke <dukeluo@outlook.com>
2019-04-15 16:13:00 +0800

    refactor: code reuse

--- week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -2,19 +2,11 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        Map<String, Integer> collectionB = new HashMap();
+        Map<String, Integer> collectionB = new cn.school.thoughtworks.section2.PracticeC().countSameElements(collectionA);
 
-        for (String s : collectionA) {
-            if (collectionB.containsKey(s)) {
-                collectionB.put(s, collectionB.get(s)+1);
-            } else {
-                collectionB.put(s, 1);
-            }
-        }
         return new PracticeB().createUpdatedCollection(collectionB, object);
     }
 }
--- week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ week06/task01/src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -2,23 +2,9 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        Map<String, Integer> collectionB = new HashMap();
-        int index;
-
-        for (String s : collectionA) {
-            index = s.indexOf("-");
-            if (index != -1) {
-                collectionB.put(s.substring(0, index), Integer.parseInt(s.substring(index+1)));
-            } else if (collectionB.containsKey(s)) {
-                collectionB.put(s, collectionB.get(s)+1);
-            } else {
-                collectionB.put(s, 1);
-            }
-        }
-        return new PracticeB().createUpdatedCollection(collectionB, object);
+        return new PracticeC().createUpdatedCollection(collectionA, object);
     }
 }
