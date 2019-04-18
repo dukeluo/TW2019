e477b7ad8acbe02028794621d8e5c60921c76e1c
duke <dukeluo@outlook.com>
2019-04-18 22:52:59 +0800

    first commit of task02 in week08

---
+++ week08/task02/src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.tdd;
+
+public class Dependency {
+    public String say(){
+        return "Leave me alone.";
+    }
+}
---
+++ week08/task02/src/main/java/com/thoughtworks/tdd/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tdd;
+
+/**
+ * Created by twer on 3/28/14.
+ */
+public class HelloWorld {
+    private Dependency dependency;
+
+    public HelloWorld(Dependency dependency) {
+
+        this.dependency = dependency;
+    }
+    public String beenCalled() {
+        return dependency.say();
+    }
+}
