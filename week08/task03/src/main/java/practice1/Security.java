e226e7a447c3b7450af4791cfa026be874608564
duke <dukeluo@outlook.com>
2019-04-19 21:54:51 +0800

    refactor(hasAccess): optimize code logic

--- week08/task03/src/main/java/practice1/Security.java
+++ week08/task03/src/main/java/practice1/Security.java
@@ -11,28 +11,11 @@ public class Security {
     }
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
-
         boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
-        }
 
-        if (permission == null) {
-            return isAccess;
-        }
-
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
+        if (securityChecker.isAdmin() || securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
             isAccess = true;
         }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
         return isAccess;
     }
 }
