0d14d3fb65a72a3fa6cc20473d2182bcc4a2ba9c
duke <dukeluo@outlook.com>
2019-04-19 11:02:42 +0800

    feat: pass test case when number is a multiple of 3

--- week08/task02/src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ week08/task02/src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,6 +2,13 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String fizzBuzz(int num) {
-        return ""+num;
+        String result = "";
+
+        if (num % 3 == 0) {
+            result += "Fizz";
+        } else {
+            result += num;
+        }
+        return result;
     }
 }
