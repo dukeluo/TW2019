0275f6ef93a95919793aa7e64e668489921be3b5
duke <dukeluo@outlook.com>
2019-04-19 10:55:57 +0800

    test: add a test case when number is not a multiple of 3, 5, 7

---
+++ week08/task02/src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void should_return_itself_when_number_is_not_a_multiple_of_3_or_5_or_7() {
+        String result = fizzBuzz.fizzBuzz(2);
+
+        assertEquals("2", result);
+    }
+}
