9aead65417c3267ef64d77a1063316d88a2395bd
duke <dukeluo@outlook.com>
2019-04-29 18:07:35 +0800

    feat: add entity of Company and Employee

--- week10/task02/src/main/java/com/example/employee/entity/Company.java
+++ week10/task02/src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,50 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private String companyName;
+    private String employeesNumber;
+
+    protected Company() {
+    }
+
+    public Company(int id, String companyName, String employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setEmployeesNumber(String employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 
 }
--- week10/task02/src/main/java/com/example/employee/entity/Employee.java
+++ week10/task02/src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,81 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    protected Employee() {
+    }
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
 }
+
